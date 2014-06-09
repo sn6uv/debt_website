d6e40b84150b0c246c67f2221c74816ff484f48a
Angus Griffith <16sn6uv@gmail.com>
2014-06-09 18:53:32 +1000

    Jarod's changes 4

--- js/main.js
+++ js/main.js
@@ -91,26 +91,86 @@ DegreeLengthSlider.on('slide', function (ev) {
 
 var TuitionFeesSlider = $('#TuitionFeesSlider').slider(sliderOptions);
 TuitionFeesSlider.on('slide', function (ev) {
-  $('#TuitionFeesBox')[0].innerHTML = (ev.value / 10.0).toFixed(0) + " %";
+  $('#TuitionFeesBox')[0].innerHTML = "$" + ((ev.value >= 100) ? "" : " ") + ev.value.toString() + " K";
   updateAll();
 });
 
 function updateFeesSlider() {
     var min, max;
-
-    // TODO fill me with correct values
     switch (parseInt(DegreeBandSelector.val())) {
-        case 1:
-          min = 0
-          max = 1000;
+        case 1: 			/*Accounting*/
+          min = 10.3 		/*10331*/
+          max = 28.3;		/*30111-1805=28306*/
           break;
+         case 2:			/*Administration*/
+          min = 10.3		/*10331*/
+          max = 28.4;		/*30111-1805=28306*/
+          break; 
+         case 4:			/*Behavioural Science*/
+          min = 7.1 		/*7100*/
+          max = 21.1;		/*30111-9033=21078*/
+          break;
+         case 6:			/*Commerce*/
+          min = 10.3 		/*10331*/
+          max = 28.3;		/*30111-1805=28306*/
+          break;
+         case 7:			/*Computing*/
+          min = 9.7 		/*9669*/
+          max = 21.1;		/*30111-9033=21078*/
+          break; 
+         case 9:			/*Economics*/
+          min = 10.3		/*10331*/
+          max = 28.3;		/*30111-1805=28306*/
+          break;
+         case 11:			/*Engineering*/
+          min = 14.0 		/*13999*/
+          max = 18.1;		/*30111-12045=18066*/
+          break; 
+         case 12:			/*Foreign Languages*/
+          min = 7.1 		/*7100*/
+          max = 13.9;		/*25984-12045=13939*/
+          break;
+         case 14:			/*Humanities*/
+          min = 6.5 		/*6458*/
+          max = 20.0;		/*25984-6021=19963*/
+          break; 
+         case 15:			/*Law*/
+          min = 10.3		/*10331*/
+          max = 28.3;		/*30111-1805=28306*/
+          break;
+         case 16:			/*Mathematics*/
+          min = 6.8 		/*6824*/
+          max = 18.1;		/*30111-12045=18066*/
+          break; 
+         case 17:			/*Medicine*/
+          min = 14.3 		/*14295*/
+          max = 12.0;		/*30111-18067=12044*/
+          break;
+         case 19:			/*Science*/
+          min = 14.0 		/*13999*/
+          max = 18.1;		/*30111-12045=18066*/
+          break;   
+         case 20:			/*Social Studies*/
+          min = 9.9 		/*9945*/
+          max = 24.1;		/*30111-6021=24090*/
+          break; 
+         case 21:			/*Statistics*/
+          min = 6.8 		/*6824*/
+          max = 18.1;		/*30111-12045=18066*/
+          break;
+         case 24:			/*Visual and Performing Arts*/
+          min = 9.3 		/*9303*/
+          max = 17.0;		/*30111-9033=16951*/
+          break;         
         default:
-          min = 0
-          max = 1000;
+          min = 10
+          max = 28;
     }
     TuitionFeesSlider.slider('setAttribute', 'min', min);
     TuitionFeesSlider.slider('setAttribute', 'max', max);
-    TuitionFeesSlider.slider('setValue', 0.5*(min + max));      // Set value to average
+    var newVal = 0.5 * (min + max);
+    TuitionFeesSlider.slider('setValue', newVal);
+    $('#TuitionFeesBox')[0].innerHTML = "$" +  newVal.toFixed(1) + " K";
 }
 
 var GapYearSlider = $('#GapYearSlider').slider(sliderOptions);
@@ -137,7 +197,7 @@ function getData() {
     'BondRate': BondRateSlider.data('slider').getValue() / 1000.0,
     'DegreeBand': parseInt($('#DegreeBandSelector').val()),
     'DegreeLength': DegreeLengthSlider.data('slider').getValue(),
-    'TuitionFees': TuitionFeesSlider.data('slider').getValue() / 1000.0,
+    'TuitionFees': TuitionFeesSlider.data('slider').getValue() * 1000.0,
     'GapYear': GapYearSlider.data('slider').getValue(),
     'StartingSalary': StartingSalarySlider.data('slider').getValue() * 1000,
     'SalaryIncrease': SalaryIncreaseSlider.data('slider').getValue() / 1000,
@@ -166,127 +226,79 @@ function updateAll() {
   switch (degree) {
   case 1:
     oldAnnualFees = 10085;
-    newContribution = 1805;
-    internationalFees = 30111;
     break;
   case 2:
     oldAnnualFees = 10085;
-    newContribution = 1805;
-    internationalFees = 30111;
     break;
   // case 3:
   //   oldAnnualFees = 8613;
-  //   newContribution = 18067;
-  //   internationalFees = 0;
   //   break;
   case 4:
     oldAnnualFees = 6044;
-    newContribution = 9033;
-    internationalFees = 30111;
     break;
   // case 5:
   //   oldAnnualFees = 8613;
-  //   newContribution = 9033;
-  //   internationalFees = 0;
   //   break;
   case 6:
     oldAnnualFees = 10085;
-    newContribution = 1805;
-    internationalFees = 30111;
     break;
   case 7:
     oldAnnualFees = 8613;
-    newContribution = 9033;
-    internationalFees = 30111;
     break;
   // case 8:
   //   oldAnnualFees = 10085;
-  //   newContribution = 18067;
-  //   internationalFees = 0;
   //   break;
   case 9:
     oldAnnualFees = 10085;
-    newContribution = 1805;
-    internationalFees = 30111;
     break;
   // case 10:
   //   oldAnnualFees = 6044;
-  //   newContribution = 9033;
-  //   internationalFees = 0;
   //   break;
   case 11:
     oldAnnualFees = 8613;
-    newContribution = 12045;
-    internationalFees = 30111;
     break;
   case 12:
     oldAnnualFees = 6044;
-    newContribution = 12045;
-    internationalFees = 25984;
     break;
   case 13:
   //   oldAnnualFees = 8613;
-  //   newContribution = 9033;
-  //   internationalFees = 0;
   //   break;
   case 14:
     oldAnnualFees = 6044;
-    newContribution = 6021;
-    internationalFees = 25984;
     break;
   case 15:
     oldAnnualFees = 10085;
-    newContribution = 1805;
-    internationalFees = 30111;
     break;
   case 16:
     oldAnnualFees = 8613;
-    newContribution = 12045;
-    internationalFees = 30111;
     break;
   case 17:
     oldAnnualFees = 10085;
-    newContribution = 18067;
-    internationalFees = 30111;
     break;
   case 18:
   //   oldAnnualFees = 6044;
-  //   newContribution = 12045;
-  //   internationalFees = 30111;
   //   break;
   case 19:
     oldAnnualFees = 8613;
-    newContribution = 12045;
-    internationalFees = 30111;
     break;
   case 20:
     oldAnnualFees = 6044;
-    newContribution = 6021;
-    internationalFees = 30111;
     break;
   case 21:
     oldAnnualFees = 8613;
-    newContribution = 12045;
-    internationalFees = 30111;
     break;
   // case 22:
   //   oldAnnualFees = 8613;
-  //   newContribution = 12045;
-  //   internationalFees = 30111;
   //   break;
   // case 23:
   //   oldAnnualFees = 10085;
-  //   newContribution = 18067;
-  //   internationalFees = 0;
   //   break;
   case 24:
     oldAnnualFees = 6044;
-    newContribution = 9033;
-    internationalFees = 25984;
     break;
   }
-  var newAnnualFees = tuitionFees * internationalFees - newContribution * Math.pow(1 + inflation, -2);
-  if (newAnnualFees < 0) {newAnnualFees = 0; }
+
+  newAnnualFees = tuitionFees;
 
   var oldFees = years * oldAnnualFees;    // total fees paid under old system (today's dollars)
   var newFees = years * newAnnualFees;    // total fees paid under new system (today's dollars)
